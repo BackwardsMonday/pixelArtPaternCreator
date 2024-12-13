187199df43a26c5d88648eac57d72e1a3fd5b68f
BackwardsMonday <wrightdaymon@gmail.com>
2024-12-13 15:28:20 -0700

    Added ability to pixilize image

---
+++ imageHandling.py
@@ -0,0 +1,70 @@
+import math
+from PIL import Image
+from PIL import ImageDraw 
+from PIL import ImageFont
+import pprint
+
+def colorDistance(c1, c2):
+    (r1,g1,b1) = c1
+    (r2,g2,b2) = c2
+    return math.sqrt(((r2-r1)*0.3)**2 + ((g2-g1)*0.59)**2 + ((b2-b1)*0.11)**2)
+
+def quantizeToPalette(silf, palette, dither=False):
+    """Convert an RGB or L mode image to use a given P image's palette."""
+
+    silf.load()
+
+    # use palette from reference image
+    palette.load()
+    if palette.mode != "P":
+        raise ValueError("bad mode for palette image")
+    if silf.mode != "RGB" and silf.mode != "L":
+        raise ValueError(
+            "only RGB or L mode images can be quantized to a palette"
+            )
+    im = silf.im.convert("P", 1 if dither else 0, palette.im)
+    # the 0 above means turn OFF dithering
+
+    # Later versions of Pillow (4.x) rename _makeself to _new
+    try:
+        return silf._new(im)
+    except AttributeError:
+        return silf._makeself(im)
+    
+def pixilizeImage(convertedImage, palette, pixelSize, changeBkg = True, bkgColor = (255,255,255), grayThreshold = 100, gray = (149,145,140)):
+    #convert image diminsions to be divisible by pixel size
+    convertedImage = convertedImage.resize((int(convertedImage.size[0]/pixelSize), int(convertedImage.size[1]/pixelSize)), Image.NEAREST)
+    convertedImage = convertedImage.resize((int(convertedImage.size[0]*pixelSize), int(convertedImage.size[1]*pixelSize)), Image.NEAREST)
+    
+    pixel = convertedImage.load()
+    for i in range(0, convertedImage.size[0], pixelSize):
+        for j in range(0, convertedImage.size[1], pixelSize):
+            color = pixel[i+(pixelSize/4), j+(pixelSize/4)]
+
+            #change background if the pixel if white or off white
+            if changeBkg and color[0] > 240 and color[1] > 240 and color[2] > 240:
+                setColor = bkgColor
+            
+            #next thing dislikes greys so deal with them first
+            #TODO: if possibile eventualy adapt this to allow for multipule grays
+            elif color[0] == color[1] and color[1] == color[2]:
+                if color[0] < grayThreshold:
+                    setColor=(0,0,0)
+                elif color[0] < 200:
+                    setColor=gray
+                else:
+                    setColor=(255,255,255)
+            else:
+                palette_r = palette[::3]
+                palette_g = palette[1::3]
+                palette_b = palette[2::3]
+                palette_rgb = zip(palette_r,palette_g,palette_b)
+                closest_colors = sorted(palette_rgb, key=lambda palette_color: colorDistance(color, palette_color))
+                setColor =  closest_colors[0]
+            for x in range(pixelSize):
+                for y in range(pixelSize):
+                    pixel[i+x,j+y] = setColor
+    return convertedImage
+
+            
+            
--- main.py
+++ main.py
@@ -1,17 +1,73 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, flash
+import imageHandling as imHand
+from PIL import Image
+from PIL import ImageDraw 
+from PIL import ImageFont
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
 
-@app.route("/successTest", methods=["POST","GET"])
-def successTest():
+IMAGE_FILES = ["png", "jpg", "jpeg"]
+UPLOAD_FOLDER = "./fileUploads"
+
+palette = [253,217,181,
+        0,0,0,
+        0,139,139,
+        31,117,254,
+        13,152,186,
+        115,102,189,
+        180,103,77,
+        255,170,204,
+        29,172,214,
+        253,219,109,
+        149,145,140,
+        28,172,120,
+        240,232,145,
+        93,118,203,
+        255,117,56,
+        238,32,77,
+        255,83,73,
+        192,68,143,
+        252,40,71,
+        146,110,174,
+        247,83,148,
+        255,255,255,
+        252,232,131,
+        197,227,132,
+        255,174,66,
+        255,255,255]
+
+#TODO: user input should detirm palette
+imPalette = Image.new("P", (24,24), 0)
+imPalette.putpalette(palette)
+
+@app.route("/fileHandeling", methods=["POST","GET"])
+def fileHandeling():
     if request.method == "POST":
+        if "file" not in request.files:
+            return "No file part"
         file = request.files["file"]
-        text = file.read(1)
+        filename = file.filename
+        if filename == "":
+            return "Plese select a file"
+        if filename.rsplit(".", 1)[1].lower() not in IMAGE_FILES:
+            return "File must be image"
+        
+        filename = secure_filename(filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
         file.close()
-        return text
+        
+        with Image.open(filename) as imFile:
+            palettedIm = imHand.quantizeToPalette(imFile, imPalette)
+            pixeledIm = imHand.pixilizeImage(palettedIm)
+            pixeledIm.save("outputs/webTest.png")
+        return "secssues"
+        
+        
     
     
 @app.route("/",methods=["POST", "GET"])
-def test():
+def mainPage():
     return render_template("uploadForm.html")
 if __name__ == '__main__':
     app.run(debug=True)
